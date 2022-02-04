0b763c0ea36aa0dbf91f405a594136506afcfaf2
eulucasm <lucaspoletis@gmail.com>
2022-02-03 22:13:37 -0300

    add layouts

---
+++ app/src/main/java/com/ciandt/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package com.ciandt.agenda.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ciandt.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
--- app/src/main/java/com/ciandt/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/ciandt/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,28 +1,29 @@
-package com.ciandt.agenda;
+package com.ciandt.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ciandt.agenda.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
+
+        setTitle("Lista de Alunos");
 
         List<String> alunos = new ArrayList<String>(Arrays.asList("Alex", "Fran", "Jose","LUCAS"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this, android.R.layout.simple_expandable_list_item_1, alunos
         ));
